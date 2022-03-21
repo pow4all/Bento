917ed34c5777bb561dc2fa1da52f8ae74af742ef
devangshekhawat <93829532+devangshekhawat@users.noreply.github.com>
2022-03-21 09:36:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -165,8 +165,8 @@ const CONFIG = {
           link: 'https://www.linkedin.com',
         },
         {
-          name: 'Dribbble',
-          link: 'https://www.dribbble.com',
+          name: 'Lichess',
+          link: 'https://www.lichess.org',
         },
         {
           name: 'Deepstash',
